d3634575794f4bb9394ee4c59748fb1fb83a2e9d
Julia Acevedo Fuentes <124298719+juliacevedofuentes@users.noreply.github.com>
2024-06-01 11:55:13 -0300

    Add files via upload

---
+++ AEDD - GUÍA PRACTICA 1 - INTRO ZINJAI - 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string nombre;
+	int edad;
+	cout << "¿Cómo te llamas? ";
+	cin >> nombre;
+	cout << "¿Cuántos años tienes? ";
+	cin >> edad;
+	cout << "¡Hola, " << nombre << "! Tienes " << edad << " años." <<
+		endl;
+	return 0;
+}
+
+
