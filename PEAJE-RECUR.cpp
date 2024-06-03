64cbe00d3d445f06400dab3bfeeff26fce3389a7
Julia Acevedo Fuentes <124298719+juliacevedofuentes@users.noreply.github.com>
2024-06-03 18:03:42 -0300

    Update PEAJE-RECUR.cpp

--- PEAJE-RECUR.cpp
+++ PEAJE-RECUR.cpp
@@ -1,50 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int peaje(int,int);
-int menos10(int,int);
+double calcularPeaje(int,int,double);
 
 int main(int argc, char *argv[]) {
-	int tipo, cant;
-	cout << "Ingrese 1 para auto, 2 para camioneta y 3 para camión. Luego ingrese cuantas veces pasa por el peaje:" << endl;
+	int tipoVehiculo, cant;
+	cout << "Ingrese 1 para auto, 2 para camioneta y 3 para camiÃ³n. Luego ingrese cuantas veces pasa por el peaje:" << endl;
 	cin >> tipo >> cant;
-	cout << "El costo total de peaje para el vehículo es de: $" << peaje(tipo,cant) << endl;
+	cout << "El costo total de peaje para el vehÃ­culo es de: $" << calcularPeaje(tipoVehiculo,cant) << endl;
 	return 0;
 }
 
-int peaje(int a,int b){
-	double x = 100;
-	double z = 150;
-	double y = 200;
-	if(b == 0){
-		return 0;
-	}else{
-		if( a == 1){
-			if(b == 1){
-			return x + peaje(a,b-1);
-			}else{
-				x = menos10(x,b);
-				return (x)+ peaje(a,b-1);
-			}
-		} else if( a == 2){
-			if(b == 1){
-				return z + peaje(a,b-1);
-			}else{
-				z = menos10(z,b);
-				return (z)+ peaje(a,b-1);
-			}
-		} else if ( a == 3){
-			if(b == 1){
-				return y + peaje(a,b-1);
-			}else{
-				y = menos10(y,b);
-				return (y)+ peaje(a,b-1);
-			}
-		} else {
-			return 0;
-		}
-	} 
-}
-int menos10(int a,int b){
-	return a-(0.1)*a;
+double calcularPeaje(int tipoVehiculo, int veces, double costoTotal = 0) {
+	double costoBase = 0;
+	if (tipoVehiculo == 1) { // Autos
+		costoBase = 100;
+	} else if (tipoVehiculo == 2) { // Camionetas
+		costoBase = 150;
+	} else if (tipoVehiculo == 3) { // Camiones
+		costoBase = 200;
+	}
+	
+	costoTotal += costoBase;
+	cout << "Vehiculo atravesando el tunel por " << veces << "ra vez. Costo total: $" << fixed << costoTotal << endl;
+	
+	if (veces > 1) {
+		double descuento = costoBase * 0.1; 
+		cout << "Descuento aplicado: $" << fixed << descuento << endl;
+		return calcularPeaje(tipoVehiculo, veces - 1, costoTotal - descuento);
+	}
+	return costoTotal;
 }
