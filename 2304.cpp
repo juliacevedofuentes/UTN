e8154fd7e878073db08559271c2901a0ec860741
Julia Acevedo Fuentes <124298719+juliacevedofuentes@users.noreply.github.com>
2024-06-04 16:08:24 -0300

    Add files via upload

---
+++ 2304.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+	
+int main(int argc, char *argv[]) {
+	int i,n;
+	cin >> i >> n;
+	int d,e,f;
+	d = e = f = i;
+	
+	while(n >= 1){
+		char mov;
+		cin >> mov;
+		if(mov == 'C'){
+			char who;
+			int much;
+			cin >> who >> much;
+			if( who == 'D'){
+				d -= much;
+			} else if( who == 'E'){
+				e -= much;
+			} else if( who == 'F'){
+				f -= much;
+			} 
+		}else if (mov == 'V'){
+			char who;
+			int much;
+			cin >> who >> much;
+			if( who == 'D'){
+				d += much;
+			} else if( who == 'E'){
+				e += much;
+			} else if( who == 'F'){
+				f += much;
+			} 
+		}else if(mov == 'A'){
+			char who,towho;
+			int much;
+			cin >> who >> towho >> much;
+			if( who == 'D'){
+				d += much;
+				if( towho == 'E'){
+					e -= much;
+				} else if( towho == 'F'){
+					f -= much;
+				} 
+			} else if( who == 'E'){
+				e += much;
+				if( towho == 'D'){
+					d -= much;
+				}  else if( towho == 'F'){
+					f -= much;
+				} 
+			} else if( who == 'F'){
+				f += much;
+				if( towho == 'D'){
+					d -= much;
+				} else if( towho == 'E'){
+					e -= much;
+				} 
+			} 
+		}
+		n--;
+	}
+	cout << d << " " << e << " " << f << endl;
+	
+	return 0;
+}
+
+
