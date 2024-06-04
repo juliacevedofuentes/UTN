49102ffc405654a56ac5112ff38c0d53f8e2bcd2
Julia Acevedo Fuentes <124298719+juliacevedofuentes@users.noreply.github.com>
2024-06-04 12:40:14 -0300

    Update PEAJE-RECUR.cpp

--- PEAJE-RECUR.cpp
+++ PEAJE-RECUR.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 double calcularPeaje(int,int,double);
@@ -6,8 +7,8 @@ double calcularPeaje(int,int,double);
 int main(int argc, char *argv[]) {
 	int tipoVehiculo, cant;
 	cout << "Ingrese 1 para auto, 2 para camioneta y 3 para camión. Luego ingrese cuantas veces pasa por el peaje:" << endl;
-	cin >> tipo >> cant;
-	cout << "El costo total de peaje para el vehículo es de: $" << calcularPeaje(tipoVehiculo,cant) << endl;
+	cin >> tipoVehiculo >> cant;
+	cout << "El costo total de peaje para el vehículo es de: $" << calcularPeaje(tipoVehiculo,cant,0) << endl;
 	return 0;
 }
 
@@ -22,12 +23,14 @@ double calcularPeaje(int tipoVehiculo, int veces, double costoTotal = 0) {
 	}
 	
 	costoTotal += costoBase;
+	cout << fixed << setprecision(2) << endl;
 	cout << "Vehiculo atravesando el tunel por " << veces << "ra vez. Costo total: $" << fixed << costoTotal << endl;
 	
 	if (veces > 1) {
 		double descuento = costoBase * 0.1; 
-		cout << "Descuento aplicado: $" << fixed << descuento << endl;
-		return calcularPeaje(tipoVehiculo, veces - 1, costoTotal - descuento);
+		cout << fixed << setprecision(2) << endl;
+		cout << "Descuento aplicado: $" << descuento << endl;
+		return calcularPeaje(tipoVehiculo, veces - 1, costoTotal - descuento*((veces-1)));
 	}
 	return costoTotal;
-}
+ }
