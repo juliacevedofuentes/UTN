77fe13060207fac83118115003a6925a0f58fa31
Julia Acevedo Fuentes <124298719+juliacevedofuentes@users.noreply.github.com>
2024-06-05 14:42:56 -0300

    Add files via upload

---
+++ TEN SQUARE ROOT.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+	using namespace std;
+	
+	double raizDeDiez(int n);
+	
+	int main(int argc, char *argv[]) {
+		int entrada;
+		cin >> entrada;
+		cout << fixed << setprecision(10) << 3 + raizDeDiez(entrada) << endl;
+		return 0;
+	}
+	
+	double raizDeDiez(int n) {
+		if (n == 0) return 0;
+		return 1/(6.0+raizDeDiez(n - 1));
+	}
+	
+	
