9314773863fe7f64a869240563c66b4aa178eb00
Julia Acevedo Fuentes <124298719+juliacevedofuentes@users.noreply.github.com>
2024-06-05 17:30:35 -0300

    Add files via upload

---
+++ Detector de letras-YAMIR.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+char tipoDeMayus(char);
+
+int main() {
+	char letra;
+	for(int i=1; i<=10; i++){
+		do{
+			cin>>letra;
+		} while(letra < 'A' or letra > 'Z');
+		
+		cout << tipoDeMayus(letra) << endl;
+	}
+	
+	return 0;
+}
+
+char tipoDeMayus(char x){
+	char tipo;
+	if(x=='A' or x=='E' or x=='I' or x=='O' or x=='U'){
+		tipo='V';
+	}
+	else if(x=='Y'){
+		tipo='S';
+	}
+	else{
+		tipo='C';
+	}
+	return tipo;
+}
+	
+	
---
+++ Detector de letras.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+bool esVocal(char);
+void salida(int,int);
+
+int main(int argc, char *argv[]) {
+	int n=0;
+	do{
+		char x;
+		cin >> x;
+		salida(x,n);
+	} while(n<=10);
+	return 0;
+}
+
+bool esVocal(char letra) {
+	if(letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U'){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+void salida(int a, int b){
+	if(a >='A' && a <= 'Z'){
+		if(esVocal(a)==1){
+			cout << "V " ;
+			b++;
+		}else if( a == 'Y'){
+			cout << "S " ;
+			b++;
+		}else{
+			cout << "C " ;
+			b++;
+		}
+	}
+}
