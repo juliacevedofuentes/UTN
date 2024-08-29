d343f7997e2fe788724aac96de0128143ef4ca96
Julia Acevedo Fuentes <124298719+juliacevedofuentes@users.noreply.github.com>
2024-08-29 00:17:05 -0300

    Add files via upload

---
+++ BEECROWD/1259 - LENTO.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+#define tr 100000
+
+void cargar_vector(int v[], int tl);
+void par_impar(int v[], int tl);
+void orden_ascedente(int par[], int tl);
+void orden_descendente(int impar[], int tl);
+void unir(int v[],int tl, int par[],int impar[], int cantpar);
+void imprimir_vector(int v[], int tl);
+
+
+int main(){
+	
+	int N;
+	int V[tr]={};
+	
+	
+	cin >> N;
+	
+	
+	cargar_vector(V, N);
+	par_impar(V, N);
+	imprimir_vector(V, N);
+	
+	return 0;
+}
+
+void cargar_vector(int v[], int tl){
+	for(int i = 0; i<tl;i++){
+		cin >> v[i];
+	}
+}
+void par_impar(int v[], int tl){
+	int par[tr]={};
+	int impar[tr]={};
+	int j = 0;
+	int k = 0;
+	for(int i = 0; i<tl;i++){
+		if(v[i]%2 == 0){
+			par[j]=v[i];
+			j++;
+		}else{
+			impar[k]=v[i];
+			k++;
+		}
+	}
+	orden_ascedente(par, j);
+	orden_descendente(impar, k);
+	unir(v,tl, par,impar,j);
+}
+void orden_ascedente(int par[], int tl){
+	int i,j=0;
+	for(i=1;i<tl;i++){
+		j=i;
+		while(j>0 and (par[j] < par[j-1])){
+			int aux = par[j];
+			par[j] = par[j-1];
+			par[j-1] = aux;
+			j = j-1;
+		}
+	}
+}
+void orden_descendente(int impar[], int tl){
+	int i,j=0;
+	for(i=1;i<tl;i++){
+		j=i;
+		while(j>0 and impar[j] > impar[j-1]){
+			int aux = impar[j];
+			impar[j] = impar[j-1];
+			impar[j-1] = aux;
+			j = j-1;
+		}
+	}
+}
+void unir(int v[], int tl, int par[], int impar[], int cantpar) {
+	int j = 0; 
+	int k = 0; 
+		
+	for(int i = 0; i < cantpar; i++) {
+		v[i] = par[j];
+		j++;
+	}
+
+	for(int i = cantpar; i < tl; i++) {
+		v[i] = impar[k];
+		k++;
+	}
+	
+}
+void imprimir_vector(int v[], int tl){
+	for( int i = 0; i< tl ; i++){
+		cout << v[i] << endl;
+	}
+}
---
+++ BEECROWD/1259 - mejora.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+#define tr 100000
+
+void cargar_vector(int v[], int tl);
+void par_impar(int v[], int tl);
+
+void quickSortdes(int v[], int inicio, int final);
+int divisiondes(int v[], int ini, int fin);
+void quickSortas(int v[], int inicio, int final);
+int divisionas(int v[], int ini, int fin);
+	
+void orden_ascedente(int par[], int tl);
+void orden_descendente(int impar[], int tl);
+void imprimir_vector(int v[], int tl);
+
+
+int main(){
+	
+	int N;
+	int V[tr]={};
+	
+	
+	cin >> N;
+	
+	
+	cargar_vector(V, N);
+	par_impar(V, N);
+	
+	return 0;
+}
+
+void cargar_vector(int v[], int tl){
+	for(int i = 0; i<tl;i++){
+		cin >> v[i];
+	}
+}
+void par_impar(int v[], int tl){
+	int par[tr]={};
+	int impar[tr]={};
+	int j = 0;
+	int k = 0;
+	for(int i = 0; i<tl;i++){
+		if(v[i]%2 == 0){
+			par[j]=v[i];
+			j++;
+		}else{
+			impar[k]=v[i];
+			k++;
+		}
+	}
+	orden_ascedente(par, j);
+	orden_descendente(impar, k);
+	
+}
+	
+	
+void quickSortdes(int v[], int inicio, int final) {
+	if (inicio < final) {
+		int pi = divisiondes(v, inicio, final);  
+		quickSortdes(v, inicio, pi - 1); 
+		quickSortdes(v, pi + 1, final);
+	}
+}
+int divisiondes(int v[], int ini, int fin) {
+	int pivot = v[fin];  
+	int i = (ini - 1);  
+	
+	for (int j = ini; j <= fin - 1; j++) {
+		
+		if (v[j] >= pivot) {
+			i++;  
+			swap(v[i], v[j]);
+		}
+	}
+	swap(v[i + 1], v[fin]);
+	return (i + 1);
+}
+void quickSortas(int v[], int inicio, int final) {
+	if (inicio < final) {
+		int pi = divisionas(v, inicio, final);  
+		quickSortas(v, inicio, pi - 1);  
+		quickSortas(v, pi + 1, final);
+	}
+}
+int divisionas(int v[], int ini, int fin) {
+	int pivot = v[fin];  
+	int i = (ini - 1);  
+	
+	for (int j = ini; j <= fin - 1; j++) {
+		if (v[j] < pivot) {
+			i++;  
+			swap(v[i], v[j]);
+		}
+	}
+	swap(v[i + 1], v[fin]);
+	return (i + 1);
+}
+
+
+void orden_ascedente(int par[], int tl){
+	quickSortas(par, 0, tl-1);
+	imprimir_vector(par, tl);
+}
+void orden_descendente(int impar[], int tl){
+	quickSortdes(impar, 0, tl-1);
+	imprimir_vector(impar, tl);
+}
+
+void imprimir_vector(int v[], int tl){
+	for( int i = 0; i< tl ; i++){
+		cout << v[i] << endl;
+	}
+}
