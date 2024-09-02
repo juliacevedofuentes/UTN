23f79e7c3e042d530a0b683ae9cdc2b64aeabbcb
Julia Acevedo Fuentes <124298719+juliacevedofuentes@users.noreply.github.com>
2024-09-02 12:10:05 -0300

    Create 1795.cpp

---
+++ BEECROWD/1795.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    long soma = pow(3,n);
+    cout << soma << endl;
+    return 0;
+}
