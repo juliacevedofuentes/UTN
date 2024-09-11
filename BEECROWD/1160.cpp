caad75f463c159df85cb1f008c422365c711a5c8
Julia Acevedo Fuentes <124298719+juliacevedofuentes@users.noreply.github.com>
2024-09-11 20:14:46 -0300

    Add files via upload

---
+++ BEECROWD/1160.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	long long t;
+	cin >> t;
+	
+	for(int i = 1; i<=t;i++){
+		long long pa,pb;
+		double g1,g2;
+		
+		cin >> pa >> pb >> g1 >> g2;
+		
+		int anos = 0;
+		int sec = 0;
+		
+		do{
+			pa += pa*g1/100;
+			pb += pb*g2/100;
+			anos++;
+			if(anos>100){
+				sec++;
+				break;
+			}
+		} while(pa<=pb);
+		
+		if(sec == 0)
+			cout << anos << " anos." << endl;
+		else
+			cout << "Mais de 1 seculo." << endl;
+	}
+	
+	return 0;
+}
---
+++ BEECROWD/2293.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+#define tm 100
+
+void cargar_matriz(int m[][tm],int cf, int cc);
+void soma_max(int m[][tm],int cf, int cc,int& max);
+
+int main()
+{
+	
+	int matriz[tm][tm] = {};
+	int n,m;
+	int maxsoma = 0;
+	cin>>n>>m;
+	
+	cargar_matriz(matriz,n,m);
+	soma_max(matriz,n,m,maxsoma);
+	
+	cout << maxsoma << endl;
+	
+	return 0;
+}
+
+void cargar_matriz(int m[][tm],int cf,int cc){
+	int i,j;
+	for (i =0; i < cf; i++){
+		for ( j=0 ; j < cc ; j++){
+			cin >> m [i][j];
+		}
+	}
+}
+void soma_max(int m[][tm],int cf, int cc,int& max){
+	int soma = 0;
+	for(int i = 0; i<cc; i++){
+		for(int j = 0; j<cf;j++){
+			soma += m[j][i];
+		}
+		if(soma > max){
+			max = soma;
+		}
+		soma = 0;
+	}
+	for(int k = 0; k<cf; k++){
+		for(int j = 0; j<cc;j++){
+			soma += m[k][i];
+		}
+		if(soma > max){
+			max = soma;
+		}
+		soma = 0;
+	}
+}
